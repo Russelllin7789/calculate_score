b99aea60038d012b4cae7fbc16660dec6310effb
Russell Lin <superbr7789@gmail.com>
2023-02-19 16:32:45 +0800

    correct present bugs

--- app.js
+++ app.js
@@ -1,24 +1,18 @@
 let hero = document.querySelector(".hero");
 let slider = document.querySelector(".slider");
 let animation = document.querySelector("section.animation-wrapper");
-let addButton = document.querySelector(".plus-btn");
-let allButtons = document.querySelectorAll("button");
-let allCredits = document.querySelectorAll(".class-credit");
-let allSelects = document.querySelectorAll("select");
-let allTrash = document.querySelectorAll(".trash-button");
-let graders = document.querySelectorAll(".grader");
-let btn1 = document.querySelector(".sort-descending");
-let btn2 = document.querySelector(".sort-ascending");
-let allInputs = document.querySelector(".all-inputs");
 
-btn1.addEventListener("click", () => handleSorting("descending"));
-btn2.addEventListener("click", () => handleSorting("ascending"));
+const time_line = new TimelineMax();
 
-const timeLine = new TimelineMax();
 // params: target, duration, original state, end state, seconds ahead
-timeLine
+time_line
   .fromTo(hero, 1, { height: "0%" }, { height: "100%", ease: Power2.easeInOut })
-  .fromTo(hero, 1.2, { width: "8%" }, { width: "100%", ease: Power2.easeInOut })
+  .fromTo(
+    hero,
+    1.2,
+    { width: "80%" },
+    { width: "100%", ease: Power2.easeInOut }
+  )
   .fromTo(
     slider,
     1,
@@ -26,87 +20,79 @@ timeLine
     { x: "0%", ease: Power2.easeInOut },
     "-=1.2"
   )
-  .fromTo(animation, 0.3, { opacity: "1" }, { opacity: "0" });
+  .fromTo(animation, 0.3, { opacity: 1 }, { opacity: 0 });
 
-window.setTimeout(() => {
+setTimeout(() => {
   animation.style.pointerEvents = "none";
 }, 2500);
 
-// disable enter key for whole project
-window.addEventListener("keypress", (event) => {
-  if (event.key === "Enter") {
-    event.preventDefault();
+// 讓整個網站的ENTER KEY都無法使用
+window.addEventListener("keypress", (e) => {
+  if (e.key == "Enter") {
+    e.preventDefault();
   }
 });
 
-// disable the submit form behavior for all buttons
-allButtons.forEach((button) =>
-  button.addEventListener("click", (event) => {
-    event.preventDefault();
-  })
-);
+// 防止FORM內部的BUTTON交出表單
+let allButtons = document.querySelectorAll("button");
+allButtons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    e.preventDefault();
+  });
+});
 
-allCredits.forEach((credit) => {
+// 選擇select內的OPTION之後，要改變相對應的顏色
+let allSelects = document.querySelectorAll("select"); // 靜態NodeList
+allSelects.forEach((select) => {
+  select.addEventListener("change", (e) => {
+    setGPA();
+    changeColor(e.target); // e.target就是<select>
+  });
+});
+
+// 改變credit之後，GPA也要更新
+let credits = document.querySelectorAll(".class-credit");
+credits.forEach((credit) => {
   credit.addEventListener("change", () => {
     setGPA();
   });
 });
 
-allSelects.forEach((select) => {
-  select.addEventListener("change", (event) => {
-    setGPA();
-    changeColor(event.target); // event.target => <select> tag itself
-  });
-});
-
-allTrash.forEach((trash) => {
-  trash.addEventListener("click", (e) => {
-    e.target.parentElement.parentElement.classList.add("remove");
-  });
-});
-allTrash.forEach((trash) => {
-  let form = trash.parentElement.parentElement;
-  form.addEventListener("transitionend", (e) => {
-    e.target.remove();
-    setGPA();
-  });
-});
-
-addButton.addEventListener("click", () => {
-  addNewForm();
-});
-
-const changeColor = (target) => {
-  if (target.value === "A" || target.value === "A-") {
+function changeColor(target) {
+  if (target.value == "A" || target.value == "A-") {
     target.style.backgroundColor = "lightgreen";
+    target.style.color = "black";
   } else if (
-    target.value === "B" ||
-    target.value === "B+" ||
-    target.value === "B-"
+    target.value == "B" ||
+    target.value == "B-" ||
+    target.value == "B+"
   ) {
     target.style.backgroundColor = "yellow";
+    target.style.color = "black";
   } else if (
-    target.value === "C" ||
-    target.value === "C+" ||
-    target.value === "C-"
+    target.value == "C" ||
+    target.value == "C-" ||
+    target.value == "C+"
   ) {
     target.style.backgroundColor = "orange";
+    target.style.color = "black";
   } else if (
-    target.value === "D" ||
-    target.value === "D+" ||
-    target.value === "D-"
+    target.value == "D" ||
+    target.value == "D-" ||
+    target.value == "D+"
   ) {
     target.style.backgroundColor = "red";
-  } else if (target.value === "F") {
+    target.style.color = "black";
+  } else if (target.value == "F") {
     target.style.backgroundColor = "grey";
     target.style.color = "white";
   } else {
     target.style.backgroundColor = "white";
   }
-};
+}
 
-const creditConverter = (creditStr) => {
-  switch (creditStr) {
+function convertor(grade) {
+  switch (grade) {
     case "A":
       return 4.0;
     case "A-":
@@ -134,56 +120,54 @@ const creditConverter = (creditStr) => {
     default:
       return 0;
   }
-};
+}
 
-const setGPA = () => {
+function setGPA() {
   let formLength = document.querySelectorAll("form").length;
+  let credits = document.querySelectorAll(".class-credit");
+  let selects = document.querySelectorAll("select");
+  let sum = 0; // GPA計算用分子
+  let creditSum = 0; // GPA計算用分母
 
-  let sum = 0;
-  let creditSum = 0;
-  let result = 0.0;
-
-  for (let i = 0; i < allCredits.length; i++) {
-    if (!isNaN(allCredits[i].valueAsNumber)) {
-      creditSum += allCredits[i].valueAsNumber;
+  for (let i = 0; i < credits.length; i++) {
+    if (!isNaN(credits[i].valueAsNumber)) {
+      creditSum += credits[i].valueAsNumber;
     }
   }
 
-  for (let i = 0; i < allSelects.length; i++) {
-    if (!isNaN(allCredits[i].valueAsNumber)) {
-      sum += allCredits[i].valueAsNumber * creditConverter(allSelects[i].value);
+  for (let i = 0; i < formLength; i++) {
+    if (!isNaN(credits[i].valueAsNumber)) {
+      sum += credits[i].valueAsNumber * convertor(selects[i].value);
     }
   }
 
-  if (creditSum !== 0) {
-    document.getElementById("result-gpa").innerText = (sum / creditSum).toFixed(
-      2
-    );
+  let result;
+  if (creditSum == 0) {
+    result = (0.0).toFixed(2);
   } else {
-    document.getElementById("result-gpa").innerText = result;
+    result = (sum / creditSum).toFixed(2);
   }
-};
+  document.getElementById("result-gpa").innerText = result;
+}
 
-const addNewForm = () => {
+let addButton = document.querySelector(".plus-btn");
+addButton.addEventListener("click", () => {
   let newForm = document.createElement("form");
   let newDiv = document.createElement("div");
   newDiv.classList.add("grader");
 
-  // add five elements within
+  // 製作五個小元素
   let newInput1 = document.createElement("input");
   newInput1.setAttribute("type", "text");
-  newInput1.setAttribute("placeholder", "class category");
   newInput1.setAttribute("list", "opt");
   newInput1.classList.add("class-type");
 
   let newInput2 = document.createElement("input");
   newInput2.setAttribute("type", "text");
-  newInput2.setAttribute("placeholder", "class number");
   newInput2.classList.add("class-number");
 
   let newInput3 = document.createElement("input");
   newInput3.setAttribute("type", "number");
-  newInput3.setAttribute("placeholder", "credits");
   newInput3.setAttribute("min", "0");
   newInput3.setAttribute("max", "6");
   newInput3.classList.add("class-credit");
@@ -191,6 +175,7 @@ const addNewForm = () => {
     setGPA();
   });
 
+  // here is the select tag
   let newSelect = document.createElement("select");
   newSelect.classList.add("select");
   var opt1 = document.createElement("option");
@@ -290,105 +275,130 @@ const addNewForm = () => {
   newDiv.appendChild(newInput3);
   newDiv.appendChild(newSelect);
   newDiv.appendChild(newButton);
+
   newForm.appendChild(newDiv);
   document.querySelector(".all-inputs").appendChild(newForm);
-
-  // add animation while new form added
   newForm.style.animation = "scaleUp 0.5s ease forwards";
-};
+});
 
-const handleSorting = (direction) => {
+let allTrash = document.querySelectorAll(".trash-button");
+allTrash.forEach((trash) => {
+  trash.addEventListener("click", (e) => {
+    e.target.parentElement.parentElement.classList.add("remove");
+  });
+});
+allTrash.forEach((trash) => {
+  let form = trash.parentElement.parentElement;
+  form.addEventListener("transitionend", (e) => {
+    e.target.remove();
+    setGPA();
+  });
+});
+
+// 排序演算法
+let btn1 = document.querySelector(".sort-descending");
+let btn2 = document.querySelector(".sort-ascending");
+btn1.addEventListener("click", () => {
+  handleSorting("descending"); // 大到小
+});
+btn2.addEventListener("click", () => {
+  handleSorting("ascending"); // 小到大
+});
+
+function handleSorting(direction) {
+  let graders = document.querySelectorAll("div.grader");
   let objectArray = [];
 
   for (let i = 0; i < graders.length; i++) {
-    let className = graders[i].children[0].value;
-    let classNumber = graders[i].children[1].value;
-    let classCredit = graders[i].children[2].value;
-    let classGrade = graders[i].children[3].value;
+    let class_name = graders[i].children[0].value; // class category
+    let class_number = graders[i].children[1].value; // class number
+    let class_credit = graders[i].children[2].value;
+    let class_grade = graders[i].children[3].value;
 
     if (
       !(
-        className === "" &&
-        classNumber === "" &&
-        classCredit === "" &&
-        classGrade === ""
+        class_name == "" &&
+        class_number == "" &&
+        class_credit == "" &&
+        class_grade == ""
       )
     ) {
-      let classObject = {
-        className,
-        classNumber,
-        classCredit,
-        classGrade,
+      let class_object = {
+        class_name,
+        class_number,
+        class_credit,
+        class_grade,
       };
-      objectArray.push(classObject);
+      objectArray.push(class_object);
     }
   }
 
+  // 取得object array後，我們可以把成績String換成數字
   for (let i = 0; i < objectArray.length; i++) {
-    objectArray[i].classGradeNumber = creditConverter(
-      objectArray[i].classGrade
-    );
+    objectArray[i].class_grade_number = convertor(objectArray[i].class_grade);
   }
 
   objectArray = mergeSort(objectArray);
-  if (direction === "descending") {
+  if (direction == "descending") {
     objectArray = objectArray.reverse();
   }
-
-  console.log("here:", direction, objectArray);
-
-  // delete all the contents within all-input area, and generate the sorted forms
+  // 根據object array的內容，來更新網頁
+  let allInputs = document.querySelector(".all-inputs");
   allInputs.innerHTML = "";
+
   for (let i = 0; i < objectArray.length; i++) {
-    allInputs.innerHTML += `
-    <form>
-            <div class="grader">
-              <input
-                type="text"
-                placeholder="class category"
-                class="class-type"
-                value="${objectArray[i].className}"
-                list="opt"
-              /><!----><input
-                type="text"
-                placeholder="class number"
-                class="class-number"
-                value="${objectArray[i].classNumber}"
-              /><!----><input
-                type="number"
-                placeholder="credits"
-                min="0"
-                max="6"
-                class="class-credit"
-                value="${objectArray[i].classCredit}"
-              /><!----><select name="select" class="select">
-                <option value=""></option>
-                <option value="A">A</option>
-                <option value="A-">A-</option>
-                <option value="B+">B+</option>
-                <option value="B">B</option>
-                <option value="B-">B-</option>
-                <option value="C+">C+</option>
-                <option value="C">C</option>
-                <option value="C-">C-</option>
-                <option value="D+">D+</option>
-                <option value="D">D</option>
-                <option value="D-">D-</option>
-                <option value="F">F</option>
-              </select><!----><button class="trash-button">
-                <i class="fas fa-trash"></i>
-              </button>
-            </div>
-          </form>
-    `;
+    allInputs.innerHTML += `<form>
+    <div class="grader">
+        <input
+        type="text"
+        placeholder="class category"
+        class="class-type"
+        list="opt"
+        value=${objectArray[i].class_name}
+        /><!--
+        --><input
+        type="text"
+        placeholder="class number"
+        class="class-number"
+        value=${objectArray[i].class_number}
+        /><!--
+        --><input
+        type="number"
+        placeholder="credits"
+        min="0"
+        max="6"
+        class="class-credit"
+        value=${objectArray[i].class_credit}
+        /><!--
+        --><select name="select" class="select">
+        <option value=""></option>
+        <option value="A">A</option>
+        <option value="A-">A-</option>
+        <option value="B+">B+</option>
+        <option value="B">B</option>
+        <option value="B-">B-</option>
+        <option value="C+">C+</option>
+        <option value="C">C</option>
+        <option value="C-">C-</option>
+        <option value="D+">D+</option>
+        <option value="D">D</option>
+        <option value="D-">D-</option>
+        <option value="F">F</option></select
+        ><!--
+        --><button class="trash-button">
+        <i class="fas fa-trash"></i>
+        </button>
+    </div>
+    </form>`;
   }
 
+  // SELECT可直接用JS更改
   graders = document.querySelectorAll("div.grader");
   for (let i = 0; i < graders.length; i++) {
-    graders[i].children[3].value = objectArray[i].classGrade;
+    graders[i].children[3].value = objectArray[i].class_grade;
   }
 
-  // add event listeners for the new elements
+  // select事件監聽
   allSelects = document.querySelectorAll("select");
   allSelects.forEach((select) => {
     changeColor(select);
@@ -398,14 +408,16 @@ const handleSorting = (direction) => {
     });
   });
 
-  allCredits = document.querySelectorAll(".class-credit");
+  // credit事件監聽
+  let allCredits = document.querySelectorAll(".class-credit");
   allCredits.forEach((credit) => {
     credit.addEventListener("change", () => {
       setGPA();
     });
   });
 
-  allTrash = document.querySelectorAll(".trash-button");
+  // 垃圾桶
+  let allTrash = document.querySelectorAll(".trash-button");
   allTrash.forEach((trash) => {
     trash.addEventListener("click", (e) => {
       e.preventDefault();
@@ -420,15 +432,15 @@ const handleSorting = (direction) => {
       );
     });
   });
-};
+}
 
-const merge = (a1, a2) => {
+function merge(a1, a2) {
   let result = [];
   let i = 0;
   let j = 0;
 
   while (i < a1.length && j < a2.length) {
-    if (a1[i].classGradeNumber > a2[j].classGradeNumber) {
+    if (a1[i].class_grade_number > a2[j].class_grade_number) {
       result.push(a2[j]);
       j++;
     } else {
@@ -447,19 +459,19 @@ const merge = (a1, a2) => {
   }
 
   return result;
-};
+}
 
-const mergeSort = (arr) => {
-  if (arr.length === 0) {
+function mergeSort(arr) {
+  if (arr.length == 0) {
     return;
   }
 
-  if (arr.length === 1) {
+  if (arr.length == 1) {
     return arr;
   } else {
-    const middle = Math.floor(arr.length / 2);
-    const left = arr.slice(0, middle);
-    const right = arr.slice(middle, arr.length);
+    let middle = Math.floor(arr.length / 2);
+    let left = arr.slice(0, middle);
+    let right = arr.slice(middle, arr.length);
     return merge(mergeSort(left), mergeSort(right));
   }
-};
+}
